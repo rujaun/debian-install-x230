59c73b9edb6e2f3b315182dd40d7a62b131f3ef8
Rujaun Fourie <rujaun@gmail.com>
2020-12-05 21:57:44 +0200

    Added unclutter

--- install_1.sh
+++ install_1.sh
@@ -38,7 +38,7 @@ cd ..
 apt -y install xorg xinit xserver-xorg-video-intel xterm xbacklight
 
 # Install some dependecies for i3block scripts
-apt -y install cpufrequtils rofi
+apt -y install cpufrequtils rofi unclutter
 
 # Install i3wm
 /usr/lib/apt/apt-helper download-file http://dl.bintray.com/i3/i3-autobuild/pool/main/i/i3-autobuild-keyring/i3-autobuild-keyring_2016.10.01_all.deb keyring.deb SHA256:460e8c7f67a6ae7c3996cc8a5915548fe2fee9637b1653353ec62b954978d844
