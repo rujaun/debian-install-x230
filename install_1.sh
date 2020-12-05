0f22aa5af1ce01c41b58889447069e12d251abbb
Rujaun Fourie <rujaun@gmail.com>
2020-12-05 11:39:51 +0200

    Added post install script #1

---
+++ install_1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+su
+
+rm /etc/apt/sources.list
+cp sources.list /etc/apt/sources.list
+
+apt update && apt install -t buster-backports linux-image-amd64 firmware-linux firmware-linux-nonfree vim htop neofetch
+
+# Enable Trim support for SSDs
+systemctl enable fstrim.trimer
+
+# Install ly login manager
+apt install build-essential libpam0g-dev libxcb-xkb-dev
+git clone https://github.com/nullgemm/ly.git
+make github
+make
+make install
+systemctl enable ly.service
+
+# Install xorg
+apt install xorg xterm
+
+# Install i3wm
+/usr/lib/apt/apt-helper download-file http://dl.bintray.com/i3/i3-autobuild/pool/main/i/i3-autobuild-keyring/i3-autobuild-keyring_2016.10.01_all.deb keyring.deb SHA256:460e8c7f67a6ae7c3996cc8a5915548fe2fee9637b1653353ec62b954978d844
+apt install ./keyring.deb
+echo 'deb http://dl.bintray.com/i3/i3-autobuild sid main' | sudo tee /etc/apt/sources.list.d/i3-autobuild.list
+echo 'Package: i3*' | sudo tee /etc/apt/preferences.d/00-i3-autobuild.pref
+echo 'Pin: origin "dl.bintray.com"' | sudo tee -a /etc/apt/preferences.d/00-i3-autobuild.pref
+echo 'Pin-Priority: 1001' | sudo tee -a /etc/apt/preferences.d/00-i3-autobuild.pref
+apt update && apt install i3
+
