9599370be31be166d39f1bf8708d06aaa4a95d5c
Rujaun Fourie <rujaun@gmail.com>
2020-12-06 12:17:11 +0200

    Refactor file names to correct order of install

--- install_4.sh
+++ install_4.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/.config/i3/
-mkdir -p ~/.config/picom/
-mkdir -p ~/.config/displays/
-mkdir -p ~/.config/i3blocks/
-mkdir -p ~/.config/i3blocks-modules/
-mkdir -p ~/.config/rofi/
+sudo cp -r config/fonts/.local.conf /etc/fonts/.local.conf
+sudo cp -r config/fonts/.fonts.conf ~/.fonts.conf
 
-cp -r config/i3/* ~/.config/i3/
-cp -r config/i3blocks/* ~/.config/i3blocks/
-cp -r config/i3blocks-modules/* ~/.config/i3blocks-modules/
-cp -r config/rofi/* ~/.config/rofi/
-cp -r config/picom/* ~/.config/picom/
-cp -r config/displays/* ~/.config/displays/
-cp -r config/vim/.vimrc ~/.vimrc
-cp -r config/tmux/.tmux.conf ~/.tmux.conf
-cp -r config/.bashrc ~/.bashrc
-cp -r config/.profile ~/.profile
-cp config/alacritty/.alacritty.yml ~/.alacritty.yml
-
-# Git
-git config --global user.name "Rujaun Fourie"
-git config --global user.email rujaun@gmail.com
-git config --global pull.rebase false
-
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install TPM for tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo mkdir /usr/share/fonts/CoreFonts
+sudo cp ./core-fonts/* /usr/share/fonts/CoreFonts/
+sudo chmod 644 /usr/share/fonts/CoreFonts/*
+fc-cache -f -v
